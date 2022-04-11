d73cfd662823c01cd21160e4ff5813151fb64dc3
Zabi Babar <zabiubabar@gmail.com>
2022-04-11 19:24:27 -0400

    Set up husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import type { NextPage } from "next";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import * as React from 'react'
+import type { NextPage } from 'next'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
 const Home: NextPage = () => {
   return (
@@ -10,10 +10,10 @@ const Home: NextPage = () => {
       <Box
         sx={{
           my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
         }}
       >
         <Typography variant="h4" component="h1" gutterBottom>
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
         </Typography>
       </Box>
     </Container>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
